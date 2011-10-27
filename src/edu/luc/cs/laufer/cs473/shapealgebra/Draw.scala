0c0957c88e614730e178c30efffad750b3f98cb4
Crane <szcshui@gmail.com>
2011-10-26 19:57:50 -0700

    update draw

--- src/edu/luc/cs/laufer/cs473/shapealgebra/Draw.scala
+++ src/edu/luc/cs/laufer/cs473/shapealgebra/Draw.scala
@@ -7,10 +7,11 @@ class Draw {
     case Ellipse(hw, hh) => g.drawArc(-hw, -hh, 2 * hw, 2 * hh, 0, 360)
     case Rectangle(w, h) => g.drawRect(0, 0, w, h)
     // TODO: Location and Group
-    case Location(x: Int, y: Int, shape: Shape) =>
+    case Location(x: Int, y: Int, shape: Shape) => {
       g.translate(x, y)
       draw(g)(shape)
       g.translate(-x, -y)
+    }
     case Group(shapes @ _*) => {
       for (s <- shapes)
         draw(g)(s)
